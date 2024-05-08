adc7bcdd42a1a45632423daff1bc3ade745e8590
Kentaro Hayashi <hayashi@clear-code.com>
2024-05-08 20:05:50 +0900

    Implement simplified chinese custom layout

--- src/virtualkeyboardpinyin.h
+++ src/virtualkeyboardpinyin.h
@@ -10,6 +10,9 @@
 #include "virtualkeyboard.h"
 #include "virtualkeyboardi18n.h"
 #include "virtualkeygeneral.h"
+#if USE_CUSTOM_LAYOUT
+#include "virtualkeyboardlayout.h"
+#endif
 
 namespace fcitx {
 namespace classicui {
@@ -17,6 +20,7 @@ namespace classicui {
 enum class PinyinKeyboardMode {
     Text,
     Mark,
+    AdditionalMark,
 };
 
 class PinyinKeyboard : public I18nKeyboard {
@@ -29,51 +33,59 @@ public:
     PinyinKeyboardMode mode() const { return mode_; }
     bool isAdditionalMarkOn() const { return isAdditionalMarkOn_; }
     void toggleMark();
+#if USE_CUSTOM_LAYOUT
+    PinyinKeyboard() : I18nKeyboard() {
+        const char *jsonPath =
+            FCITX_INSTALL_PKGDATADIR "/addon/virtualkeyboardui-zh_CN.json";
+        FCITX_KEYBOARD() << "path of Korean keyboard layout file: " << jsonPath;
+        loader_ = new KeyboardLayout(jsonPath);
+    }
+#endif
 
 private:
+#if USE_CUSTOM_LAYOUT
+    void setLayerKeys(size_t offset);
+    KeyboardLayout *loader_;
+#else
     void setTextKeys();
     void setMarkKeys();
     void setAdditionalMarkKeys();
+#endif
     PinyinKeyboardMode mode_ = PinyinKeyboardMode::Text;
     bool isAdditionalMarkOn_ = false;
 };
 
 class PinyinSpaceKey : public SpaceKey {
 public:
-    PinyinSpaceKey() {
-        setFontSize(18);
-    }
-    const char* label(VirtualKeyboard *keyboard) const override;
+    PinyinSpaceKey() { setFontSize(18); }
+    const char *label(VirtualKeyboard *keyboard) const override;
 };
 
 class PinyinEnterKey : public EnterKey {
 public:
-    PinyinEnterKey() {
-        setFontSize(18);
-    }
-    const char* label(VirtualKeyboard *keyboard) const override;
+    PinyinEnterKey() { setFontSize(18); }
+    const char *label(VirtualKeyboard *keyboard) const override;
 };
 
 class PinyinModeSwitchKey : public SwitchKey {
 public:
     PinyinModeSwitchKey() : SwitchKey() {}
-    const char* label(VirtualKeyboard *) const override { return "A#"; }
+    const char *label(VirtualKeyboard *) const override { return "A#"; }
 
 protected:
     int numberOfStates() const override { return 2; }
     const char *stateLabel(int index) const override {
         return index == 0 ? "A" : "#";
     }
-    void switchState(VirtualKeyboard *keyboard, InputContext *inputContext) override;
+    void switchState(VirtualKeyboard *keyboard,
+                     InputContext *inputContext) override;
     int currentIndex(VirtualKeyboard *keyboard) override;
 };
 
 class PinyinMarkToggleKey : public ToggleKey {
 public:
-    PinyinMarkToggleKey() {
-        setFontSize(18);
-    }
-    const char* label(VirtualKeyboard *) const override { return "更多"; }
+    PinyinMarkToggleKey() { setFontSize(18); }
+    const char *label(VirtualKeyboard *) const override { return "更多"; }
 
 protected:
     void toggle(VirtualKeyboard *keyboard, InputContext *inputContext) override;
